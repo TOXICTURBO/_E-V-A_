7a3a4dc97ab3a6687697c247cfb829651c491042
Toxic-Turbo <97358745+TOXICTURBO@users.noreply.github.com>
2022-05-01 17:15:30 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -11,9 +11,9 @@ const defaultMenu = {
      ✪〔 %me 〕✪
 🔸Hai, %name!
 
-🔸 Github:https://github.com/SudoAnirudh
+🔸 Github:https://github.com/TURBOHYPER
 
-🔸Instagram:https://www.instagram.com/__a_n_i_r_u_d_h_/
+🔸Instagram:https://www.instagram.com/toxic_turbo777
 
      〘 Eva 〙
 %readmore`.trimStart(),
@@ -345,7 +345,7 @@ let handler = async (m, { conn, usedPrefix: _p, args, command }) => {
       readmore: readMore
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
-    await conn.send2ButtonLoc(m.chat, await (await fetch("https://telegra.ph/file/e32f2651bac64b6d62c81.jpg")).buffer(), text.trim(), 'Made With ❤️ by Anirudh', 'Owner Bot', ',owner', 'All Commands', '.? all', m)
+    await conn.send2ButtonLoc(m.chat, await (await fetch("https://telegra.ph/file/e32f2651bac64b6d62c81.jpg")).buffer(), text.trim(), 'Made With ❤️ by Turbo', 'Owner Bot', ',owner', 'All Commands', '.? all', m)
   } catch (e) {
     conn.reply(m.chat, 'Sorry, Try Again', m)
     throw e
