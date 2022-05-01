059451dfc2c9dde04da3d602b12965d0a40c3b48
Toxic-Turbo <97358745+TOXICTURBO@users.noreply.github.com>
2022-05-01 17:19:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 let fs = require('fs')
-global.owner = ['919539102851'] // Letakan nomor kamu disini
+global.owner = ['916380260672'] // Letakan nomor kamu disini
 global.mods = [] // Moderator?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // Pengguna premium tidak memerlukan limit
 global.APIs = { // API Prefix
@@ -31,14 +31,14 @@ global.APIKeys = { // APIKey nya disini
 }
 
 // Sticker WM
-global.packname = 'Eva-Sticker'
-global.author = 'Eva'
+global.packname = 'By'
+global.author = 'Turbo'
 
 global.wait = '_*Please Wait..*_'
 global.eror = '_*Server Error*_'
 global.fla = 'https://ibb.co/jW31z7n'
 
-global.watermark = '© EVA'
+global.watermark = '© Turbo'
 
 global.multiplier = 69 // Semakin tinggi, semakin sulit naik level
 
